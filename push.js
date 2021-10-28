923c2733b394fccfdf6df4bbd2b853f86b2e8e3c
Alexlm-17 <alexandrolm17@gmail.com>
2021-10-28 20:34:19 -0300

    Insert the elements in the first position with method (push)

---
+++ push.js
@@ -0,0 +1,14 @@
+// insert the elements in the first position
+const numbers = [1,2,3,4,5,6,7,8,9,10]
+
+numbers[numbers.length] = 11
+
+console.log(numbers);
+
+// Insert the elements int the first position with Method PUSH
+
+numbers.push(12)
+console.log(numbers);
+
+numbers.push(13,14,15,15)
+console.log(numbers);
