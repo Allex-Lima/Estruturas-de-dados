4c83745ce46e601630be89d126fa0265996f7146
Alexandro <alexlmangabeira@gmail.com>
2021-11-04 21:35:27 -0300

    file correcting

--- remove-of-the-end/pop.js
+++ remove-of-the-end/pop.js
@@ -3,16 +3,10 @@
 
 const numbers = [1,2,3,4,5]
 
-let novo = []
-
-numbers.pop()
-
-console.log(numbers);
-
 numbers.pop()
 console.log(numbers);
 
-for (let i = 0; i < numbers.length; i++) {
-    numbers[i] = numbers[i + 1]
+for (let i = numbers.length - 1; i >= 0; i--) {
+    numbers[i] = numbers[i - 1]
 }
 console.log(numbers);
