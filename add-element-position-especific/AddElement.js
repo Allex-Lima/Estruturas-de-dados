f178111372e8907261e5383ec864150eda9b47e3
Alexandro <alexlmangabeira@gmail.com>
2021-11-18 20:02:16 -0300

    Removendo elemento na posição específica e visualizando o Array no console

---
+++ add-element-position-especific/AddElement.js
@@ -0,0 +1,24 @@
+/* 
+Achando elemento a ser removido na posição específica
+e visualizando o Array no console
+*/
+
+const numbers = [1,2,3,4,5]
+
+const elementRemoved = 1
+let pos
+for(let i = 0; i < numbers.length; i++) {
+    if(numbers[i] === elementRemoved) {
+        pos = i
+    }
+    numbers[pos] = numbers[i + 1]
+}
+
+const newArray = []
+
+for(let i = 0; i < numbers.length; i++) {
+    if(numbers[i] !== undefined) {
+        newArray.push(numbers[i])
+    }
+}
+console.log(newArray);
